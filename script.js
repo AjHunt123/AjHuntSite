8503d955f69d5d926d0a0796db729368aedd0fba
AjHunt123 <145989216+AjHunt123@users.noreply.github.com>
2024-03-06 19:02:25 +0800

    Update script.js
    direct fix

--- script.js
+++ script.js
@@ -16,13 +16,3 @@ window.onscroll = () => {
         }
     });
 };
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  
